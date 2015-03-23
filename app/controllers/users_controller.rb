7a78a269627442be5e400cc0d47ec5de733ed6f9
unique56 <japanseler@gmail.com>
2015-03-23 15:59:17 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   
   def index
@@ -38,6 +38,20 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end 
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   
   private
     def user_params
